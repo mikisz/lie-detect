1bc84fb67a4f6f6283df8823b81d316bbae905ba
Mikołaj Niżnik <mikolaj.niznik@gmail.com>
2025-12-09 14:55:02 +0100

    Initial Commit

---
+++ lie-detect/lie_detectApp.swift
@@ -0,0 +1,17 @@
+//
+//  lie_detectApp.swift
+//  lie-detect
+//
+//  Created by Mikołaj Niżnik on 09/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct lie_detectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
